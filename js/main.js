fa3ef3ec6c458219d6c0508e045ee855b2ce14bb
vmetelev <vmetelev@carrot.software>
2025-04-17 16:36:30 +0300

    vault backup: 2025-04-17 16:36:30

---
+++ js/main.js
@@ -0,0 +1,17 @@
+  window.$docsify = {
+    name: "Cassandra.lite-invest",
+    loadSidebar: true,
+    relativePath: true,
+    maxLevel: 4,
+    subMaxLevel: 3,
+  
+    versions: docsifyVersions,
+    versionSelectorLabel: "Version",
+  
+    search: {
+      noData: "Нет результатов!",
+      paths: "auto",
+      placeholder: "Поиск",
+      hideOtherSidebarContent: true,
+    },
+  };
