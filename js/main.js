82dede5c7c723c704d656596eadab5ae4eeeb78b
metelev <metelev@oviont.ru>
2025-12-22 16:15:36 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -6,6 +6,23 @@
   },
     subMaxLevel: 0,
 
+    plugins: [
+      function (hook, vm) {
+        hook.ready(function () {
+          if (!location.hash || location.hash === '#/') {
+            fetch('_sidebar.md')
+              .then(res => res.text())
+              .then(md => {
+                const match = md.match(/\((weeks\/[^)]+)\)/);
+                if (match) {
+                  vm.router.replace('/' + match[1]);
+                }
+              });
+          }
+        });
+      }
+    ],
+
     search: {
       noData: "Нет результатов!",
       paths: "auto",
